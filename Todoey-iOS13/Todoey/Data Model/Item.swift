c991666f7235eb4aff9b39740a42b4160dfda349
AbdullahNanaDVT <79845386+AbdullahNanaDVT@users.noreply.github.com>
2021-06-23 13:49:44 +0200

    Working using NSCoder and plists instead of UserDefaults

---
+++ Todoey-iOS13/Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Abdullah Nana on 2021/06/22.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
